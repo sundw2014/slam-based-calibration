6eda6e0b40de974ae2c666a40aad47d45df2c3ef
sundw <sundw2014@gmail.com>
2017-08-15 14:22:01 +0800

    fix cv2.circle u,v bug

--- lidar_camera_calibration_slam_based/scripts/visualize_fuse.py
+++ lidar_camera_calibration_slam_based/scripts/visualize_fuse.py
@@ -33,7 +33,8 @@ class image_converter:
     # cv2.imshow("Image window", cv_image)
     # cv2.waitKey(3)
 
-_T_cam_velo = np.array([0.0, -0.0583, -0.015, -1.57, 0, -1.57])
+_T_cam_velo = np.array([0.0, -0.0583, -0.015, 1.57, -1.35, 0.0])
+# _T_cam_velo = np.array([-0.47637765, -0.07337429, -0.33399681, -2.8704988456, -1.56405382877, -1.84993623057])
 fx = 1358.010977
 fy = 1357.44302436
 cx = 950.243
@@ -41,13 +42,15 @@ cy = 644.85837
 camera_K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
 image_w = 1920; image_h = 1200
 
-def transform_show(_T_cam_velo, local_image):
+def transform_show(_T_cam_velo, local_image, frameId):
     T_cam_velo = transformation.compose_matrix(angles = _T_cam_velo[3:6], translate = _T_cam_velo[0:3])
-    filename = '/home/sundw/data/velodyne_points/770.bin'
+    filename = '/home/sundw/workspace/data/2017_08_14/velodyne_points/' + str(frameId) + '.bin'
     velo = np.fromfile(filename, dtype=np.float32)
     velo = velo.reshape((-1, 4)).transpose()
-    transformed_pl = T_cam_velo.dot(velo)
-    image_points = camera_K.dot(transformed_pl[0:3, :])
+    velo[3,:] = 1
+    # print(velo[:,0:5])
+    transformed_pl = T_cam_velo[0:3, :].dot(velo)
+    image_points = camera_K.dot(transformed_pl)
     local_image = local_image.copy()
     for i in range(image_points.shape[1]):
         image_point = image_points[:, i]
@@ -57,9 +60,9 @@ def transform_show(_T_cam_velo, local_image):
         u = int(u/z); v = int(v/z);
         if u<0 or u>=image_w or v<0 or v>=image_h or z<0:
             continue
-        cv2.circle(local_image, (v,u), 3, 255)
-    cv2.imshow('fuse', cv2.resize(local_image, (0, 0), fx = 0.5, fy = 0.5))
-    cv2.waitKey(30)
+        cv2.circle(local_image, (u,v), 3, 255)
+    cv2.imshow('fuse', local_image) #cv2.resize(local_image, (0, 0), fx = 0.5, fy = 0.5))
+    cv2.waitKey(0)
 
 def main(args):
   ic = image_converter(args[1])
@@ -69,24 +72,6 @@ def main(args):
         global image
         if image is not None:
             from IPython import embed; embed()
-            filename = '/home/sundw/data/velodyne_points/770.bin'
-            velo = np.fromfile(filename, dtype=np.float32)
-            velo = velo.reshape((-1, 4)).transpose()
-            image_w = 1920; image_h = 1200
-            transformed_pl = T_cam_velo.dot(velo)
-            image_points = camera_K.dot(transformed_pl[0:3, :])
-            local_image = image.copy()
-            for i in range(image_points.shape[1]):
-                image_point = image_points[:, i]
-                u = image_point[0]
-                v = image_point[1]
-                z = image_point[2]
-                u = int(u/z); v = int(v/z);
-                if u<0 or u>=image_w or v<0 or v>=image_h or z<0:
-                    continue
-                cv2.circle(local_image, (v,u), 3, 255)
-            cv2.imshow('fuse', cv2.resize(local_image, (0, 0), fx = 0.5, fy = 0.5))
-            cv2.waitKey(100)
             break;
         time.sleep(1)
   except KeyboardInterrupt:
